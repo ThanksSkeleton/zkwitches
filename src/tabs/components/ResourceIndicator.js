1c8afaa5c1c643bf6df4b834e7c9eae36d549cde
ThanksSkeleton <Thanks.Skeleton@gmail.com>
2022-04-25 11:17:40 -0700

    remove unused file

--- src/tabs/components/ResourceIndicator.js
+++
@@ -1,8 +0,0 @@
-import Backdrop from "@mui/material/Backdrop";
-import CircularProgress from "@mui/material/CircularProgress";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Stack from '@mui/material/Stack';
-import Chip from '@mui/material/Chip';
-
-
